d0b91630487d8364aa84abc4ea6e2a58c46e1443
sergey <sergey@javat.Dlink>
2018-02-25 21:38:26 +0200

    Topic 2. Apache Tails.

--- src/main/java/com/caveofprogramming/App.java
+++ src/main/java/com/caveofprogramming/App.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
 
 @SpringBootApplication
 public class App extends SpringBootServletInitializer {
@@ -19,5 +23,22 @@ public class App extends SpringBootServletInitializer {
 		return application.sources(App.class);
 	}
 
+	@Bean
+    public UrlBasedViewResolver tilesViewResolver() {  
+      UrlBasedViewResolver tilesViewResolver = new UrlBasedViewResolver();  
+   
+      tilesViewResolver.setViewClass(TilesView.class);  
+      return tilesViewResolver;  
+    }
+	
+	 @Bean
+	    public TilesConfigurer tilesConfigurer(){
+	        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+	        String[] defs = {"/WEB-INF/tiles.xml"};
+	        tilesConfigurer.setDefinitions(defs);
+	        //tilesConfigurer.setCheckRefresh(true);
+	        return tilesConfigurer;
+	    }
+	 
 	
 }
